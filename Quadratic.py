ab11d29058e54e3718f27931aacbb6c7f79ad42a
solas2529 <ishaan.kelkar@gmail.com>
2024-05-14 21:31:55 -0500

    Add files via upload

---
+++ Quadratic.py
@@ -0,0 +1,35 @@
+
+
+import math
+print("Enter quadratic letters in order")
+a = float(input("enter A number"))
+b = float(input("enter B number"))
+c = float(input("enter C number"))
+
+
+if (a == 0.0):
+    print("Not quadratic")
+elif (a > 0):
+    if (b*b - 4*a*c < 0):
+        print("There are no zeroes")
+    else:
+        zero1 = ((-b+math.sqrt(b*b - 4*a*c))/(2*a))
+        zero2 = ((-b-math.sqrt(b*b - 4*a*c))/(2*a))
+        print("Zeroes are at " + str(zero1) + "and " + str(zero2))
+    min = ((-b)/(2*a))
+    point = (a*min*min) + (b * min) + c
+    print("Minimum is at (" + str(min) + ", " + str(point) + ")")
+
+elif (a > 0):
+    if (b*b - 4*a*c < 0):
+        print("There are no zeroes")
+    else:
+        zero1 = ((-b+math.sqrt(b*b - 4*a*c))/(2*a))
+        zero2 = ((-b-math.sqrt(b*b - 4*a*c))/(2*a))
+        print("Zeroes are at " + str(zero1) + " and " + str(zero2))
+    max = ((-b)/(2*a))
+    point = (a*max*max) + (b * max) + c
+    print("Minimum is at ()" + str(max) + ", " + str(point) + ")")
+
+print("Acceleration is " + str(2 * a))
+
